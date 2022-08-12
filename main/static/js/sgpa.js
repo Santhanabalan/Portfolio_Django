1e78806253c673a071b3db4b1b07e3e6897f96a2
Santhanabalan <bala2102002@gmail.com>
2022-08-12 18:14:00 +0530

    Revert "Temp commit"
    This reverts commit e8a7e8dbaa7d55426820e490c24f2818f3c6be7a.

--- main/static/js/sgpa.js
+++ main/static/js/sgpa.js
@@ -1,11 +1,24 @@
-function addinputFields(){
-    var number = document.getElementById("sub").value;
-    for (i=1;i<number;i++){
-        var elem = document.querySelector('#clone'+i);
-        var clone = elem.cloneNode(true);
-        clone.id = 'clone'+(i+1);
-        elem.after(clone);
-    }
-    // document.getElementById(count).style.display = 'none';
-    
-}
+$("#addRow").click(function () {
+    var html = '';
+    html += '<div class="form-row">';
+    html += '<div class="form-group col-md-5">';
+    html += '<label for="inputZip">Course Credit</label>';
+    html += '<input type="number" class="form-control" id="cat1" placeholder="Total Credit">';
+    html += '</div>';
+    html += '<div class="form-group col-md-5">';
+    html += '<label for="inputZip">Course Grade</label>';
+    html += '<input type="number" class="form-control" id="cat2" min="0" max="10" placeholder="Out of 10">';
+    html += '</div>';
+    html += '<div class="form-group col-md-2">';
+    html += '<label for="inputZip">Remove</label>';
+    html += '<button button id="removeRow" type="button" class="form-control btn btn-danger ">Remove</button>';
+    html += '</div>';  
+    html += '</div>';
+
+    $('#newRow').append(html);
+});
+
+// remove row
+$(document).on('click', '#removeRow', function () {
+    $(this).closest('.form-row').remove();
+});
