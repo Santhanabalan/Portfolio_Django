563d751cdaa7aa9898a2d5d6e46bd317064dfb76
Santhanabalan <bala2102002@gmail.com>
2022-08-14 17:28:31 +0530

    Added Login Required Decorators

--- Portfolio_Django/settings.py
+++ Portfolio_Django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9wz6(mzkq40i-&(t_#mtve5h!hgt98#gpj05zyd^s=2mhaye7c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sb2portfolio.herokuapp.com','127.0.0.1','localhost']
 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from main.forms import ContactForm
+from allauth.account.decorators import login_required
 
 def index (request):
     if request.method == 'POST':
@@ -17,18 +18,23 @@ def certificates (request):
 def portfolio (request): 
     return render(request, 'main/portfolio-details.html')
 
+@login_required(login_url='/accounts/login') 
 def dashboard (request):
     return render(request, 'main/dashboard.html')
 
+@login_required(login_url='/accounts/login') 
 def sgpa (request):
     return render(request, 'main/sgpa.html')
 
+@login_required(login_url='/accounts/login') 
 def cgpa (request):
     return render(request, 'main/cgpa.html')
 
+@login_required(login_url='/accounts/login') 
 def gpp (request):
     return render(request, 'main/gpp.html')
 
+@login_required(login_url='/accounts/login') 
 def temp (request):
     return render(request, 'main/temp.html')
 
