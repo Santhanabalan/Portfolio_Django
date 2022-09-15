6f0c9ac5c32fc713632557e54ec1d7af8d7bc886
Santhanabalan <bala2102002@gmail.com>
2022-09-15 22:27:44 +0530

    debug = false

--- Portfolio_Django/settings.py
+++ Portfolio_Django/settings.py
@@ -23,7 +23,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['portfolio.santhanabalan.ml','127.0.0.1','localhost']
 CSRF_TRUSTED_ORIGINS = ['https://portfolio.santhanabalan.ml']
