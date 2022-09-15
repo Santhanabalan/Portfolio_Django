c5cc8e632e21610c7b21b420a145742dd6edc76d
Santhanabalan <bala2102002@gmail.com>
2022-09-15 18:20:26 +0530

    Added Static root

--- Portfolio_Django/settings.py
+++ Portfolio_Django/settings.py
@@ -142,10 +142,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'templates/static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
