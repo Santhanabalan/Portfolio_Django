0cba553516b20937a2fb01a55775bf2a7360ea47
Santhanabalan <bala2102002@gmail.com>
2022-09-19 18:07:45 +0530

    Cleaning Repo

--- staticfiles/js/gpp.d5e67e1bbc51.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('form').hide();
-  $('#theory').show();
-  $('#inputState').change(function () {
-      $('form').hide();
-      $('#'+$(this).val()).show();})
-});
--- staticfiles/js/sgc.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#sgpa").submit(function() {
-        // Get all the forms elements and their values in one step
-        var values = $(this).serialize();
-        console.log(values);
-    });
-});
