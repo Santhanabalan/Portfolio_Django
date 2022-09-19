d6a4f4e31f00ebf5b33b8c42ad30acddf474333f
Santhanabalan <bala2102002@gmail.com>
2022-09-19 19:37:06 +0530

    Whitenoise revert

--- Portfolio_Django/settings.py
+++ Portfolio_Django/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'templates/static'
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
