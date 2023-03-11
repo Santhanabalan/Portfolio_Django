323cd6fb90cc4d60eeee623e70fc8bfc3ef3c1df
Santhanabalan-gitkraken <admin@santhanabalan.ml>
2023-03-11 19:07:22 +0530

    Dynamicall render Projects and Certificates
    -Remove hard coded divs

--- main/models.py
+++ main/models.py
@@ -11,20 +11,34 @@ class Contact(models.Model):
         return self.email
     
 class Project(models.Model):
+    FILTER = (
+        ('web','Web'),
+        ('app','App'),
+        ('aiml','AI/ML'),
+    )
     name = models.CharField(max_length=255)
     description = models.TextField()
     img_src = models.URLField()
     project_link = models.URLField()
-    tag = models.CharField(max_length=255)
+    tag = models.CharField(max_length=200,choices=FILTER)
 
     def __str__(self):
         return self.name
     
 class Certificate(models.Model):
+    FILTER = (
+        ('cs','Coursera'),
+        ('udm','UDEMY'),
+        ('kct','KCT'),
+        ('fl','FutureLearn'),
+        ('gl','Great Learning'),
+        ('aws','AWS'),
+        ('ms','Microsoft'),
+    )
     name = models.CharField(max_length=255)
     img_src = models.URLField()
     credential = models.URLField()
-    tag = models.CharField(max_length=255)
+    tag = models.CharField(max_length=200,choices=FILTER)
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from .forms import ContactForm
-from .models import Contact,Project,Certificate
+from .models import Project,Certificate
 # Create your views here.
 def index (request):
+    projects=Project.objects.all()
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
             return render(request, 'main/index.html')
     form = ContactForm()
-    context = {'form': form}
+    context = {'projects':projects,'form': form}
     return render(request, 'main/index.html', context)
 
-def certificates (request): 
-    return render(request, 'main/certificates.html')
+def certificates (request):
+    certificates=Certificate.objects.all()
+    context = {'certificates':certificates}
+    return render(request, 'main/certificates.html', context)
 
 def portfolio_details (request): 
     return render(request, 'main/portfolio-details.html')
