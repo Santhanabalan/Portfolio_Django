16d8d5951d844eccb134b2bf569e0e6638455346
Santhanabalan-gitkraken <admin@santhanabalan.ml>
2023-03-17 23:23:23 +0530

    Fix jQuery load
    - jQuery need to load before the our js file...

--- dashboard/static/assets/js/sgc.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $( "#addRow").click(function(){
-    var x = $(form).serializeArray();
-    $.each(x, function(i, field) {
-    alert(field.name + " : " + field.value + " ");
-    });
-    });
-    });
